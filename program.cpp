94e4a9b46da48ae44d65e109d79b97611c167aa6
unknown <goran.ilioski@students.finki.ukim.mk>
2019-04-16 15:50:30 +0200

    functions implemented

--- program.cpp
+++ program.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 void printMyName () {
 	//TODO
+	cout("Goran Ilioski");
 }
 
 void printMyIndex () {
 	//TODO
+	cout("151102");
 }
 
 int main () {
 	//TODO
+	printMyName();
+	printMyIndex();
 }
